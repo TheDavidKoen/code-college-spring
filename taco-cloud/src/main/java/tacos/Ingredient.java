ace0d21ea860317ecf16be2900db66832d5f8f40
David Koen <davidodawie@gmail.com>
2024-11-01 12:27:53 +0200

    added config properties.

--- taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -14,13 +14,13 @@ import lombok.RequiredArgsConstructor;
 @Entity
 public class Ingredient {
 
-    @Id
-    private final String id;
-    private final String name;
-    private final Type type;
+  @Id
+  private final String id;
+  private final String name;
+  private final Type type;
 
-    public enum Type {
-        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-    }
+  public enum Type {
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  }
 
 }
--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,9 +1,11 @@
 package tacos.web;
-
 import javax.validation.Valid;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,49 +23,89 @@ import tacos.data.OrderRepository;
 @SessionAttributes("order")
 public class OrderController {
 
-    private OrderRepository orderRepo;
+  private OrderRepository orderRepo;
 
-    public OrderController(OrderRepository orderRepo) {
-        this.orderRepo = orderRepo;
+  private OrderProps props;
+
+  public OrderController(OrderRepository orderRepo,
+          OrderProps props) {
+    this.orderRepo = orderRepo;
+    this.props = props;
+  }
+
+  @GetMapping("/current")
+  public String orderForm(@AuthenticationPrincipal User user,
+      @ModelAttribute TacoOrder order) {
+    if (order.getDeliveryName() == null) {
+      order.setDeliveryName(user.getFullname());
+    }
+    if (order.getDeliveryStreet() == null) {
+      order.setDeliveryStreet(user.getStreet());
+    }
+    if (order.getDeliveryCity() == null) {
+      order.setDeliveryCity(user.getCity());
+    }
+    if (order.getDeliveryState() == null) {
+      order.setDeliveryState(user.getState());
+    }
+    if (order.getDeliveryZip() == null) {
+      order.setDeliveryZip(user.getZip());
     }
 
-    @GetMapping("/current")
-    public String orderForm(@AuthenticationPrincipal User user,
-                            @ModelAttribute TacoOrder order) {
-        if (order.getDeliveryName() == null) {
-            order.setDeliveryName(user.getFullname());
-        }
-        if (order.getDeliveryStreet() == null) {
-            order.setDeliveryStreet(user.getStreet());
-        }
-        if (order.getDeliveryCity() == null) {
-            order.setDeliveryCity(user.getCity());
-        }
-        if (order.getDeliveryState() == null) {
-            order.setDeliveryState(user.getState());
-        }
-        if (order.getDeliveryZip() == null) {
-            order.setDeliveryZip(user.getZip());
-        }
+    return "orderForm";
+  }
 
-        return "orderForm";
+  @PostMapping
+  public String processOrder(@Valid TacoOrder order, Errors errors,
+      SessionStatus sessionStatus,
+      @AuthenticationPrincipal User user) {
+
+    if (errors.hasErrors()) {
+      return "orderForm";
     }
 
-    @PostMapping
-    public String processOrder(@Valid TacoOrder order, Errors errors,
-                               SessionStatus sessionStatus,
-                               @AuthenticationPrincipal User user) {
+    order.setUser(user);
 
-        if (errors.hasErrors()) {
-            return "orderForm";
-        }
+    orderRepo.save(order);
+    sessionStatus.setComplete();
 
-        order.setUser(user);
+    return "redirect:/";
+  }
 
-        orderRepo.save(order);
-        sessionStatus.setComplete();
+  @GetMapping
+  public String ordersForUser(
+      @AuthenticationPrincipal User user, Model model) {
 
-        return "redirect:/";
-    }
+    Pageable pageable = PageRequest.of(0, props.getPageSize());
+    model.addAttribute("orders",
+        orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+
+    return "orderList";
+  }
+
+  /*
+  @GetMapping
+  public String ordersForUser(
+      @AuthenticationPrincipal User user, Model model) {
+
+    Pageable pageable = PageRequest.of(0, 20);
+    model.addAttribute("orders",
+        orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+
+    return "orderList";
+  }
+   */
+
+  /*
+  @GetMapping
+  public String ordersForUser(
+      @AuthenticationPrincipal User user, Model model) {
+
+    model.addAttribute("orders",
+        orderRepo.findByUserOrderByPlacedAtDesc(user));
+
+    return "orderList";
+  }
+   */
 
 }
