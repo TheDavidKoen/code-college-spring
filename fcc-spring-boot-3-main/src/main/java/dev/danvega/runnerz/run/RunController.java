55c20809d1cc9098152049dcb36b66f26b522845
David Koen <davidodawie@gmail.com>
2024-11-14 09:01:12 +0200

    added swagger.

--- fcc-spring-boot-3-main/src/main/java/dev/danvega/runnerz/run/RunController.java
+++ fcc-spring-boot-3-main/src/main/java/dev/danvega/runnerz/run/RunController.java
@@ -1,5 +1,7 @@
 package dev.danvega.runnerz.run;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "Run APIs", description = "Here are the requests you can send your run data.")
 @RestController
 @RequestMapping("/api/runs")
 class RunController {
@@ -18,11 +21,19 @@ class RunController {
         this.runRepository = runRepository;
     }
 
+    @Operation(
+            summary = "Retrieve all runs.",
+            description = "Get all existing runs.",
+            tags = { "API", "get" })
     @GetMapping
     List<Run> findAll() {
         return runRepository.findAll();
     }
 
+    @Operation(
+            summary = "Retrieve a run by Id",
+            description = "Get a run object by specifying its id.",
+            tags = { "API", "get" })
     @GetMapping("/{id}")
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
@@ -32,18 +43,30 @@ class RunController {
         return run.get();
     }
 
+    @Operation(
+            summary = "Post a run",
+            description = "Post a new run.",
+            tags = { "API", "post" })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
+    @Operation(
+            summary = "Edit an existing run by Id",
+            description = "Edit a run object by specifying its id.",
+            tags = { "API", "put" })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run,id);
     }
 
+    @Operation(
+            summary = "Delete a run by Id",
+            description = "Remove a run object by specifying its id.",
+            tags = { "API", "delete" })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     void delete(@PathVariable Integer id) {
--- fcc-spring-boot-3-main/src/main/java/dev/danvega/runnerz/user/UserController.java
+++ fcc-spring-boot-3-main/src/main/java/dev/danvega/runnerz/user/UserController.java
@@ -1,5 +1,6 @@
 package dev.danvega.runnerz.user;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "User APIs", description = "Here are the requests you can send your user data.")
 @RestController
 @RequestMapping("/users")
 class UserController {
